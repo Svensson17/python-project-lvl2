21026fab8f602f73720f40e7e99c355c3ee5c17a
Svennson17 <kiryl.shvedau@gmail.com>
2021-10-10 13:14:45 +0300

    step3 fix

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -2,8 +2,22 @@ import json
 
 
 def generate_diff(filename1, filename2):
-    first_file = json.load(open(filename1))
-    second_file = json.load(open(filename2))
-    print(first_file, second_file)
+    data1 = json.load(open(filename1))
+    data2 = json.load(open(filename2))
+    print(data1, data2)
+    keys = set(list(data1.keys()) + list(data2.keys()))
+    result = "{\n"
+    for key in sorted(keys):
+        if key not in data2:
+            result += " - {}: {}\n".format(key, data1[key])
+        elif key not in data1:
+            pass
+        elif data1[key] != data2[key]:
+            pass
+        else:
+            pass
+        result += "}"
+    return result
+
 
     
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,10 +2,12 @@ from gendiff.get_difference import get_difference
 
 from gendiff.generate_diff import generate_diff
 
+
 def main():
     args = get_difference()
     generate_diff(args.first_argument, args.second_argument)
-    print("dgdgdgdgdgdgdgd")
+    result = generate_diff()
+    print(result)
 
 
 if __name__ == "__main__":
