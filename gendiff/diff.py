fc2febb5c947ca442f0d65d503bc1dfa93c1fe43
Svennson17 <kiryl.shvedau@gmail.com>
2021-10-25 12:29:06 +0300

    step6

---
+++ gendiff/diff.py
@@ -0,0 +1,55 @@
+def build_diff(data1, data2):
+    return {
+        "type": "origin",
+        "children": create(data1, data2)
+    }
+
+def create(data1, data2=None):
+    if not isinstance(data1, dict):
+        return data1
+    if data2 is None:
+        data2 = data1
+    keys = create_list_keys(data1, data2)
+    result = []
+    for key in sorted(keys):
+        if key not in data1:
+            result.append({
+                "type": "added",
+                "key": key,
+                "value": data2[key]
+            })
+        elif key not in data2:
+            result.append({
+                "type": "removed",
+                "key": key,
+                "value": data1[key]
+            })
+        elif isinstance(data1[key], dict):
+            if isinstance(data2[key], dict):
+                result.append({
+                    "type": "nested",
+                    "key": key,
+                    "children": create(data1[key], data2[key])
+                })
+        elif data1[key] != data2[key]:
+            result.append({
+                "type": "updated",
+                "key": key,
+                "old_value": data1[key],
+                "new_value": data2[key]
+            })
+        else:
+            result.append({
+                "type": "unchanged",
+                "key": key,
+                "value": data1[key]
+            })
+    return result
+
+
+
+
+def create_list_keys(data1, data2):
+    keys1 = list(data1.keys())
+    keys2 = list(data2.keys())
+    return keys1 if (data1 == data2) else set(keys1 + keys2)
--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,24 +1,39 @@
 import json
 
+import os
+
+import yaml
+
+from gendiff.diff import build_diff
+from gendiff.formator import formator
+
 
 def generate_diff(filename1, filename2):
-    data1 = json.load(open(filename1))
-    data2 = json.load(open(filename2))
-    print(data1, data2)
-    keys = set(list(data1.keys()) + list(data2.keys()))
-    result = "{\n"
-    for key in sorted(keys):
-        if key not in data2:
-            result += "  - {}: {}\n".format(key, data1[key])
-        elif key not in data1:
-            result += "  + {}: {}\n".format(key, data2[key])
-        elif data1[key] != data2[key]:
-            result += "  - {}: {}\n".format(key, data1[key])
-            result += "  + {}: {}\n".format(key, data2[key])
-        else:
-            result += "    {}: {}\n".format(key, data1[key])
-    result += "}"
-    return result
+    _, extension = os.path.splitext(filename1)
+    if extension == ".json":
+        data1 = json.load(open(filename1))
+        data2 = json.load(open(filename2))
+    else:
+        data1 = yaml.safe_load(open(filename1))
+        data2 = yaml.safe_load(open(filename2))
+    #
+    # print(data1, data2)
+    # keys = set(list(data1.keys()) + list(data2.keys()))
+    # result = "{\n"
+    # for key in sorted(keys):
+    #     if key not in data2:
+    #         result += "  - {}: {}\n".format(key, data1[key])
+    #     elif key not in data1:
+    #         result += "  + {}: {}\n".format(key, data2[key])
+    #     elif data1[key] != data2[key]:
+    #         result += "  - {}: {}\n".format(key, data1[key])
+    #         result += "  + {}: {}\n".format(key, data2[key])
+    #     else:
+    #         result += "    {}: {}\n".format(key, data1[key])
+    # result += "}"
+    diff = build_diff(data1, data2)
+    return formator(diff, 'stylish')
+
 
 
     
