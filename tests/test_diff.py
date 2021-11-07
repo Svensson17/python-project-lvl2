a4f4e5d1fdf0c55a27d864b187bb580fb6c32690
Svennson17 <kiryl.shvedau@gmail.com>
2021-11-07 13:12:58 +0200

    step8

---
+++ tests/test_diff.py
@@ -0,0 +1,8 @@
+import pytest
+
+from gendiff import generate_diff
+
+def test_gendiff_stylish():
+    path_file1 = './tests/fixtures/file1.json'
+    path_file1 = './tests/fixtures/file2.json'
+    assert generate_diff(path_file1,path_file2) == open('./tests/fixtures/result_stylish', 'r').read
