bb2cb0bb10df14ec397e409fa03be6bec2ad8d48
Svennson17 <kiryl.shvedau@gmail.com>
2021-11-11 19:16:12 +0200

    step8 fix

--- tests/test_diff_plain.py
+++ tests/test_diff_plain.py
@@ -5,4 +5,5 @@ from gendiff.generate_diff import generate_diff
 def test_gendiff_plain():
     path_file1 = './tests/fixtures/file1.yml'
     path_file2 = './tests/fixtures/file2.yml'
-    assert generate_diff(path_file1,path_file2) == open('./tests/fixtures/result_plain', 'r').read()
+    output_format = 'plain'
+    assert generate_diff(path_file1, path_file2, output_format) == open('./tests/fixtures/result_plain', 'r').read()
