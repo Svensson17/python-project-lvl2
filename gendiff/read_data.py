f28c9ee2900d7d0941fc9341abf73fe6a4bf5068
Svennson17 <kiryl.shvedau@gmail.com>
2021-11-14 16:03:39 +0200

    step8 fix

--- gendiff/read_data.py
+++ gendiff/read_data.py
@@ -14,6 +14,5 @@ def read_data(path):
 def parse(data, format_type):
     if format_type == ".json":
         return json.load(data)
-    elif:
-        format_type in {'.yml', '.yaml'}
+    elif format_type in {'.yml', '.yaml'}:
         return yaml.safe_load(data)
