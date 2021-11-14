72a7192e9250b000d085878b0cee0b0beb618c29
Svennson17 <kiryl.shvedau@gmail.com>
2021-11-14 15:59:01 +0200

    step8 fix

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,3 +1,5 @@
+import json
+
 from gendiff.generate_diff import generate_diff
 
 
@@ -7,6 +9,26 @@ def test_gendiff_stylish():
     assert generate_diff(path_file1, path_file2) == open('./tests/fixtures/result_stylish', 'r').read()
 
 
-def test_diff_plain():
+def test_gendiff_plain():
+    path_file1 = './tests/fixtures/file1.yml'
+    path_file2 = './tests/fixtures/file2.yml'
+    output_format = 'plain'
+    assert generate_diff(
+        path_file1,
+        path_file2,
+        output_format
+    ) == open(
+        './tests/fixtures/'
+        'result_plain',
+        'r'
+    ).read()
+
+
+def test_diff_json():
     path_file1 = './tests/fixtures/file1.json'
     path_file2 = './tests/fixtures/file2.json'
+    data = generate_diff(path_file1, path_file2)
+    try:
+        json.load(data)
+    except:
+        print('Invalid json')
--- tests/test_diff_json.py
+++
@@ -1,6 +0,0 @@
-from gendiff.generate_diff import generate_diff
-
-
-def test_diff_plain():
-    path_file1 = './tests/fixtures/file1.json'
-    path_file2 = './tests/fixtures/file2.json'
